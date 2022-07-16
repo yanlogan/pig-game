5704945008d0a18fa2b0b7c24a852f3a92263acb
Yan Logan <yanlogans@gmail.com>
2022-07-16 20:51:42 +0300

    feat: add rolling the dice and updated game reset

--- script.js
+++ script.js
@@ -3,16 +3,54 @@
 const players = document.querySelectorAll('.player');
 const playerScores = document.querySelectorAll('.score');
 const playerScoresCurrent = document.querySelectorAll('.current-score');
-const dice = document.querySelector('.dice');
+const diceImg = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+
+let activePlayer = 0;
+const scores = [0, 0];
+let currentScore = 0;
+
+const showActivePlayer = () => {
+  for (let i = 0; i < players.length; i++) {
+    players[i].classList.remove('player--active');
+  }
+  players[activePlayer].classList.add('player--active');
+};
+
+const updateCurrentScore = () => {
+  playerScoresCurrent[activePlayer].textContent = currentScore;
+};
+
+const switchPlayer = () => {
+  activePlayer = activePlayer === 0 ? 1 : 0;
+};
 
 const resetGame = () => {
   for (let i = 0; i < players.length; i++) {
     playerScores[i].textContent = 0;
     playerScoresCurrent[i].textContent = 0;
   }
-  dice.classList.add('hidden');
-  if (!players[0].classList.contains('player--active'))
-    players[0].classList.add('player--active');
+  diceImg.classList.add('hidden');
+  activePlayer = 0;
+  showActivePlayer();
 };
 
 resetGame();
+
+btnNew.addEventListener('click', resetGame);
+
+btnRoll.addEventListener('click', () => {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (diceImg.classList.contains('hidden')) diceImg.classList.remove('hidden');
+  diceImg.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    updateCurrentScore();
+  } else {
+    currentScore = 0;
+    updateCurrentScore();
+    switchPlayer();
+    showActivePlayer();
+  }
+});
