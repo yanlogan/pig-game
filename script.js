e438b5c42734691952974ec36c19087c8752fb43
Yan Logan <yanlogans@gmail.com>
2022-07-16 20:09:34 +0300

    feat: add resetGame function

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const players = document.querySelectorAll('.player');
+const playerScores = document.querySelectorAll('.score');
+const playerScoresCurrent = document.querySelectorAll('.current-score');
+const dice = document.querySelector('.dice');
+
+const resetGame = () => {
+  for (let i = 0; i < players.length; i++) {
+    playerScores[i].textContent = 0;
+    playerScoresCurrent[i].textContent = 0;
+  }
+  dice.classList.add('hidden');
+  if (!players[0].classList.contains('player--active'))
+    players[0].classList.add('player--active');
+};
+
+resetGame();
